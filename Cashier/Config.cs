0d4457d3fcbc3bdcb726f62739293a60515494aa
Fabiann227 <hannfarhan9@gmail.com>
2023-11-06 09:12:45 +0700

    first commit

---
+++ Cashier/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier
+{
+    internal class Config
+    {
+        public string Server { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+
+        // Properti statis untuk mengakses instansi tunggal
+        private static Config _instance;
+        public static Config Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Config();
+                }
+                return _instance;
+            }
+        }
+    }
+}
